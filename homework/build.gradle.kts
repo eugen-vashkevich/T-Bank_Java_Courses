286b17b5218a7ccb26e46cf9322b4537af466fcf
eugen <6739118@gmail.com>
2024-08-31 22:37:57 +0300

    Added jackson-databind dependency

--- homework/build.gradle.kts
+++ homework/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     compileOnly("org.projectlombok:lombok:1.18.34")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
